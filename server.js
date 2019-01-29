6ea645cb9796759752891df33b5fdbcc00d49e6c
Cole Vanacore <colevanacore@gmail.com>
2019-01-29 14:15:13 -0700

    Finish get endpoints

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.listen(app.get('port'), () => {
   console.log(`BYOB is running on ${app.get('port')}`)
 })
 
+//get all venues
 app.get('/api/v1/venues', (request, response) => {
   database('venues').select()
     .then(venues => {
@@ -23,10 +24,35 @@ app.get('/api/v1/venues', (request, response) => {
     })
 })
 
+//get all concerts
 app.get('/api/v1/concerts', (request, response) => {
   database('concerts').select()
     .then(concerts => {
       response.status(200).json(concerts)
+    })
+    .catch(error => {
+      response.status(500).json({error})
+    })
+})
+
+//get specific venue by id
+app.get('/api/v1/venues/:id', (request, response) => {
+  const id = request.param('id');
+  database('venues').where('id', id)
+    .then(venue => {
+      response.status(200).json(venue)
+    })
+    .catch(error => {
+      response.status(500).json({error})
+    })
+})
+
+//get specific concert
+app.get('/api/v1/concerts/:id', (request, response) => {
+  const id = request.param('id');
+  database('concerts').where('id', id)
+    .then(concert => {
+      response.status(200).json(concert)
     })
     .catch(error => {
       response.status(500).json({error})
