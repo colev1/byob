fed90e5680fcaa4056dcbc521b97c9cc4a199a45
Cole Vanacore <colevanacore@gmail.com>
2019-01-29 17:27:47 -0700

    Setup all tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -8,10 +8,80 @@ const database = require('knex')(configuration);
 
 chai.use(chaiHttp);
 
-describe('Client Routes', () => {
-
-});
-
 describe('API Routes', () => {
+  before(done => {
+    database.migrate.rollback()
+    .then(() => {
+      database.migrate.latest()
+    })
+    .then(() => {
+       database.seed.run()
+    })
+    .then(() => {
+      done();
+    })
+  });
+  
+  beforeEach(done => {
+    database.migrate.rollback()
+    .then(() => {
+      database.migrate.latest()
+    })
+    .then(() => {
+      database.seed.run()
+    })
+    .then(() => {
+      done();
+    });
+  });
 
+  describe('/api/v1/venues', () => {
+    it('GET: should return all of the venues', () => {
+
+    })
+
+    it('posts successfully', () => {
+      
+    })
+    it('post sad path', () => {
+      
+    })
+  })
+
+  describe('/api/v1/concerts', () => {
+    it('GET: should return all of the concerts', () => {
+
+    })
+
+    it('posts successfully', () => {
+      
+    })
+
+    it('post sad path', () => {
+      
+    })
+  })
 });
+
+
+describe('/api/v1/venues/:id', () => {
+  it('PUT: should edit venue data', () => {
+
+  })
+
+  it('deletes a venue', () => {
+
+  })
+})
+
+describe('/api/v1/concert/:id', () => {
+  
+  it('PUT: should edit concert data', () => {
+
+  })
+
+  it('deletes a concert', () => {
+
+  })
+})
+
