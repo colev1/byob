78f4c35a3c81ca80f8e92f8a35a5ac4770853e41
Cole Vanacore <colevanacore@gmail.com>
2019-01-31 11:58:02 -0700

    Add seeds to knex

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/test'
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
